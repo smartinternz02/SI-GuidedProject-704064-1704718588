50abed177df9daa59e207621ae9d41a45b2a8791
upendrabudi <155978698+upendrabudi@users.noreply.github.com>
2024-01-29 17:26:05 +0530

    Add files via upload

---
+++ Test Suites/CURA_TestSuiteCollection/CURA TestSuiteCollection MakeAppointment 002.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>CURA TestSuiteCollection MakeAppointment 002</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>PARALLEL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/CURA_TestSuiteCollection/CURA_TestSuites/Cura_TestSuite_MakeAppointment_002</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
